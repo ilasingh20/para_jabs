1a41bc62ae28bf27bf368d1784e90b52d1f00e9d
ilasingh20 <ns.ilasingh@gmail.com>
2024-07-24 17:40:45 +0530

    coderefactor

---
+++ src/main/java/com/example/para_jobs/ParaJobsApplication.java
@@ -0,0 +1,13 @@
+package com.example.para_jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParaJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParaJobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/para_jobs/ParaJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.para_jobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParaJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
